315c630830df48b34791067cdb319d3253558bfe
Umit  Mester <umitmester@Umits-MacBook-Pro.local>
2024-03-28 09:07:18 -0400

    update

--- stock-app/src/Gecode.js
+++ stock-app/src/Gecode.js
@@ -1,10 +1,10 @@
 // address  => lat, long
 // lat, long => address
-import Geocode from "react-geocode";
-Geocode.setApiKey("AIzaSyD8IPCxZZMLEmwmGb02HXPb6RsdJ8EMvk8");
-Geocode.setLanguage("en");
-Geocode.setLocationType("ROOFTOP");
-Geocode.enableDebug();
+// import Geocode from "react-geocode";
+// Geocode.setApiKey("AIzaSyD8IPCxZZMLEmwmGb02HXPb6RsdJ8EMvk8");
+// Geocode.setLanguage("en");
+// Geocode.setLocationType("ROOFTOP");
+// Geocode.enableDebug();
 
 // // Get latitude & longitude from address.
 // Geocode.fromAddress("Eiffel Tower").then(
@@ -18,8 +18,25 @@ Geocode.enableDebug();
 // );
 
 
-export async function geocode(address){
-    const res = await Geocode.fromAddress(address)
-    const { lat, lng } = res.results[0].geometry.location;
-    console.log(lat, lng);
+// export async function geocode(address){
+//     const res = await Geocode.fromAddress(address)
+//     const { lat, lng } = res.results[0].geometry.location;
+//     console.log(lat, lng);
+// }
+
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyD8IPCxZZMLEmwmGb02HXPb6RsdJ8EMvk8");
+Geocode.setLanguage("en");
+Geocode.setLocationType("ROOFTOP");
+Geocode.enableDebug();
+
+export async function geocode(address) {
+    try {
+        const res = await Geocode.fromAddress(address);
+        const { lat, lng } = res.results[0].geometry.location;
+        console.log(lat, lng);
+    } catch (error) {
+        console.error(error);
+    }
 }
